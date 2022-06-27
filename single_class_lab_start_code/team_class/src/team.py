7bd8cb67a50345d62da8ce3f2d543b96912cfc7b
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-06-27 13:19:36 +0100

    1,2,3 passed

---
+++ single_class_lab_start_code/team_class/src/team.py
@@ -0,0 +1,6 @@
+class Team:
+    def __init__(self, input_name, input_players, input_coach):
+        self.name = input_name
+        self.players = input_players
+        self.coach = input_coach
+    
